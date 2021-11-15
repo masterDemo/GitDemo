1a7309602eb0f3d6f08eda52620b6760d0b7a53f
aiswarya <sreelathaaiswarya@gmail.com>
2021-11-16 00:56:41 +0530

    first commit......

---
+++ src/packageDemo/Test1.java
@@ -0,0 +1,27 @@
+package packageDemo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		Test1 t1=new Test1();
+		int c=t1.add(10,20);
+		System.out.println("the sum is "+c);
+		
+
+	}
+
+	public int add(int a, int b)
+	{
+		int c=a+b;
+		return c;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
