1ed578385ff5388297180a35ce73bd9bd76ff7df
aiswarya <sreelathaaiswarya@gmail.com>
2021-11-16 09:48:28 +0530

    Commiting Test2 to BranchX

---
+++ src/packageDemo/Test2.java
@@ -0,0 +1,23 @@
+package packageDemo;
+
+public class Test2 {
+
+
+	public static void main(String[] args) {
+		
+		Test1 t1=new Test1();
+		int c=t1.add(10,20);
+		System.out.println("the sum is "+c);
+		
+
+	}
+
+	public int add(int a, int b)
+	{
+		int c=a+b;
+		return c;
+	}
+	
+	
+	
+}
